435e4a4dd88969e81fcde1a771c7ce8aae39aa6d
Leonid <44239176+robert-werner@users.noreply.github.com>
2025-03-04 22:49:00 +0300

    Update container_build-binary.sh

--- scripts/debian/container_build-binary.sh
+++ scripts/debian/container_build-binary.sh
@@ -70,7 +70,7 @@ cd "$dir_build/$package_name-$version_kernel"
 mk-build-deps -ir -t 'apt-get -y'
 
 echo "[INFO ] Building binary package for $release"
-$schedtool dpkg-buildpackage --build=binary
+$schedtool dpkg-buildpackage -j30 -uc -us --build=binary
 
 echo "[INFO ] Copying binary packages to bind mount: $dir_artifacts/"
 mkdir -p "$dir_artifacts"
